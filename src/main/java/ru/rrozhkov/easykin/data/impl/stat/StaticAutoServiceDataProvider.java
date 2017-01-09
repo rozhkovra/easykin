5175fe540b858e2c7502f9b39d47539d0db26a23
rozhkovra <rozhkovra@gmail.com>
2017-01-09 09:30:51 +0600

    Add new tasks and auto services data

--- src/main/java/ru/rrozhkov/easykin/data/impl/stat/StaticAutoServiceDataProvider.java
+++ src/main/java/ru/rrozhkov/easykin/data/impl/stat/StaticAutoServiceDataProvider.java
@@ -1,24 +1,23 @@
 package ru.rrozhkov.easykin.data.impl.stat;
 
-import static ru.rrozhkov.easykin.model.auto.service.impl.ServiceFactory.createDetail;
-import static ru.rrozhkov.easykin.model.auto.service.impl.ServiceFactory.createRepairService;
-import static ru.rrozhkov.easykin.model.auto.service.impl.ServiceFactory.createService;
-
 import java.util.Arrays;
 import java.util.Date;
 
 import ru.rrozhkov.easykin.data.impl.CollectionDataProvider;
 import ru.rrozhkov.easykin.model.auto.service.IService;
+import ru.rrozhkov.easykin.model.auto.service.impl.ServiceFactory;
 import ru.rrozhkov.easykin.model.fin.Money;
 import ru.rrozhkov.easykin.util.CollectionUtil;
+import ru.rrozhkov.easykin.util.DateUtil;
 
 public class StaticAutoServiceDataProvider extends CollectionDataProvider<IService>{
 	
 	public StaticAutoServiceDataProvider(){
         super(CollectionUtil.copy(Arrays.asList(
-            	createService("Заправка", new Money(1000.0), new Date())
-            	,createRepairService("Замена масла", new Money(350.50), new Date(),Arrays.asList(createDetail("Масло",new Money(1500.0), new Date())))
-            	,createRepairService("Замена масляного фильтра", new Money(0.0), new Date(), Arrays.asList(createDetail("Масляный фильтр",new Money(450.0), new Date()))))
+            	ServiceFactory.createService("Заправка", new Money(1000.0), new Date())
+            	,ServiceFactory.createRepairService("Замена масла", new Money(350.50), new Date(),Arrays.asList(ServiceFactory.createDetail("Масло",new Money(1500.0), new Date())))
+            	,ServiceFactory.createRepairService("Замена масляного фильтра", new Money(0.0), new Date(), Arrays.asList(ServiceFactory.createDetail("Масляный фильтр",new Money(450.0), new Date())))
+            	,ServiceFactory.createRepairService("Замена свечей", new Money(0.0), DateUtil.parse("04.01.2017"), Arrays.asList(ServiceFactory.createDetail("Свечи NGK 16мм",new Money(580.0), DateUtil.parse("04.01.2017")),ServiceFactory.createDetail("Свечной ключ на 16мм",new Money(190.0), DateUtil.parse("03.01.2017")))))
             	));
 	}
 }
