093f3e1637fa7829867f4bc9a54430542903ec22
Roman Rozhkov <rrozhkov@luxoft.com>
2017-01-18 06:12:41 -0430

    Return all tasks

--- src/main/java/ru/rrozhkov/easykin/context/EasyKinContext.java
+++ src/main/java/ru/rrozhkov/easykin/context/EasyKinContext.java
@@ -6,9 +6,6 @@ import ru.rrozhkov.easykin.db.CategoryHandler;
 import ru.rrozhkov.easykin.db.TaskHandler;
 import ru.rrozhkov.easykin.model.category.ICategory;
 import ru.rrozhkov.easykin.model.task.ITask;
-import ru.rrozhkov.easykin.model.task.Status;
-import ru.rrozhkov.easykin.model.task.impl.filter.TaskFilterFactory;
-import ru.rrozhkov.easykin.util.FilterUtil;
 
 public class EasyKinContext {
 	private Collection<ICategory> categories;
@@ -20,7 +17,7 @@ public class EasyKinContext {
 	
 	public EasyKinContext init(){
 		this.categories = CategoryHandler.selectCategories();
-		this.tasks = FilterUtil.filter(TaskHandler.selectTasks(),TaskFilterFactory.createStatusFilter(Status.OPEN));
+		this.tasks = TaskHandler.selectTasks();
 		return this;
 	}
 
