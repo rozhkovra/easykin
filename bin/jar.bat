3c7b811f492cfd862c924f16365ed5c59c163c65
Roman Rozhkov <rrozhkov@luxoft.com>
2017-02-05 12:11:22 +0600

    Refactoring

--- bin/jar.bat
+++ bin/jar.bat
@@ -1,4 +1,5 @@
 ﻿REM
 cd ..\target\classes
 jar cf ..\..\jar\easykin-gui.jar ru\rrozhkov\easykin\gui\*.*
-jar cf ..\..\jar\easykin.jar ru\rrozhkov\easykin\*.* ru\rrozhkov\easykin\context\*.* ru\rrozhkov\easykin\data\*.* ru\rrozhkov\easykin\db\*.* ru\rrozhkov\easykin\filter\*.* ru\rrozhkov\easykin\model\*.* ru\rrozhkov\easykin\util\*.*
+jar cf ..\..\jar\easykin.jar ru\rrozhkov\easykin\context\*.* ru\rrozhkov\easykin\data\*.* ru\rrozhkov\easykin\db\*.* ru\rrozhkov\easykin\model\*.* ru\rrozhkov\easykin\util\*.*
+copy  ..\..\jar\easykin.jar ..\..\..\EasyKinWeb\src\main\webapp\WEB-INF\lib\
