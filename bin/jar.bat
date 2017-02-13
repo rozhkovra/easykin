3f84adc67b80d854c5ff2886b227af545ecf3277
Roman Rozhkov <rrozhkov@luxoft.com>
2017-02-13 15:02:35 +0600

    Add ws package

--- bin/jar.bat
+++ bin/jar.bat
@@ -2,4 +2,5 @@
 cd ..\target\classes
 jar cf ..\..\jar\easykin-gui.jar ru\rrozhkov\easykin\gui\*
 jar cf ..\..\jar\easykin.jar ru\rrozhkov\easykin\context\* ru\rrozhkov\easykin\data\* ru\rrozhkov\easykin\db\* ru\rrozhkov\easykin\model\* ru\rrozhkov\easykin\util\*
+jar cf ..\..\jar\easykin-ws.jar ru\rrozhkov\easykin\ws\*
 copy  ..\..\jar\easykin.jar ..\..\..\EasyKinWeb\src\main\webapp\WEB-INF\lib\
