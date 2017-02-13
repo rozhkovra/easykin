2723400c17ab383256e8433dd7a8e2c30f516804
Roman Rozhkov <rrozhkov@luxoft.com>
2017-02-13 18:47:58 +0600

    Import project to IntelliJ IDEA

--- src/main/java/ru/rrozhkov/easykin/ws/EasyKinPublisher.java
+++ src/main/java/ru/rrozhkov/easykin/ws/EasyKinPublisher.java
@@ -4,6 +4,7 @@ import javax.xml.ws.Endpoint;
 
 public class EasyKinPublisher {
 	public static void main(String[] args){
+		System.out.println("Publish EasyKinService: http://localhost:8081/EasyKinService/");
 		Endpoint.publish("http://localhost:8081/EasyKinService/", new EasyKinService());
 	}
 }
