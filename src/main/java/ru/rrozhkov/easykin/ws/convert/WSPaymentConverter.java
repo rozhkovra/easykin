b7137a450156c862a1d45c707631a18f65914063
Roman Rozhkov <rrozhkov@luxoft.com>
2017-02-22 12:17:34 +0600

    Refactoring

--- src/main/java/ru/rrozhkov/easykin/ws/convert/WSPaymentConverter.java
+++ src/main/java/ru/rrozhkov/easykin/ws/convert/WSPaymentConverter.java
@@ -2,11 +2,7 @@ package ru.rrozhkov.easykin.ws.convert;
 
 import ru.rrozhkov.easykin.model.fin.payment.IPayment;
 import ru.rrozhkov.easykin.model.fin.payment.PaymentStatus;
-import ru.rrozhkov.easykin.model.task.ITask;
-import ru.rrozhkov.easykin.model.task.Priority;
-import ru.rrozhkov.easykin.model.task.Status;
 import ru.rrozhkov.easykin.ws.bean.PaymentBean;
-import ru.rrozhkov.easykin.ws.bean.TaskBean;
 import ru.rrozhkov.lib.convert.IConverter;
 
 public class WSPaymentConverter implements IConverter<IPayment, PaymentBean> {
