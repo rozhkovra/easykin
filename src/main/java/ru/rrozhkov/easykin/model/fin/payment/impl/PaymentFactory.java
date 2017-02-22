6bfe123bc4984b2519e53aa192976ff19a899d85
Roman Rozhkov <rrozhkov@luxoft.com>
2017-02-22 12:16:48 +0600

    Refactoring

--- src/main/java/ru/rrozhkov/easykin/model/fin/payment/impl/PaymentFactory.java
+++ src/main/java/ru/rrozhkov/easykin/model/fin/payment/impl/PaymentFactory.java
@@ -10,26 +10,30 @@ import java.util.Date;
 
 import ru.rrozhkov.easykin.model.fin.Money;
 import ru.rrozhkov.easykin.model.fin.payment.IPayment;
+import ru.rrozhkov.easykin.model.fin.payment.PaymentCategory;
 import ru.rrozhkov.easykin.model.fin.payment.PaymentStatus;
 
 public class PaymentFactory {
 	public static IPayment createAutoPayment(String comment, Money amount, Date date){
-		return new Payment(AUTO, comment,amount,date,PaymentStatus.FACT);
+		return createPayment(AUTO, comment,amount,date,PaymentStatus.FACT);
 	}
 	public static IPayment createAutoRepairPayment(String comment, Money amount, Date date){
-		return new Payment(AUTOREPAIR, comment,amount,date,PaymentStatus.FACT);
+		return createPayment(AUTOREPAIR, comment,amount,date,PaymentStatus.FACT);
 	}
 	public static IPayment createDetailPayment(String comment, Money amount, Date date){
-		return new Payment(AUTODETAIL, comment,amount,date,PaymentStatus.FACT);
+		return createPayment(AUTODETAIL, comment,amount,date,PaymentStatus.FACT);
 	}
 	public static IPayment createServiceCalcPayment(String comment, Money amount, Date date, boolean isPaid){
 		if(isPaid)
-			return new Payment(SERVICE, comment,amount,date,PaymentStatus.FACT);
-		return new Payment(SERVICE, comment,amount,date,PaymentStatus.PLAN);
+			return createPayment(SERVICE, comment,amount,date,PaymentStatus.FACT);
+		return createPayment(SERVICE, comment,amount,date,PaymentStatus.PLAN);
 	}
 	public static IPayment createTaskPayment(String comment, Money amount, Date date, ru.rrozhkov.easykin.model.task.Status status){
 		if(status.isClose())
-			return new Payment(TASK, comment,amount,date,PaymentStatus.FACT);
-		return new Payment(TASK, comment,amount,date,PaymentStatus.PLAN);
+			return createPayment(TASK, comment,amount,date,PaymentStatus.FACT);
+		return createPayment(TASK, comment,amount,date,PaymentStatus.PLAN);
+	}
+	public static IPayment createPayment(PaymentCategory category, String comment, Money amount, Date date, PaymentStatus status){
+		return new Payment(category, comment,amount,date,status);
 	}
 }
