93363d95e162016d66181fce8a2576bc3ece6421
Roman Rozhkov <rrozhkov@luxoft.com>
2017-02-28 13:40:47 +0600

    Refactoring

--- src/main/java/ru/rrozhkov/easykin/ws/task/EasyKinTaskPublisher.java
+++ src/main/java/ru/rrozhkov/easykin/ws/task/EasyKinTaskPublisher.java
@@ -1,10 +1,8 @@
-package ru.rrozhkov.easykin.ws;
-
-import ru.rrozhkov.easykin.ws.task.EasyKinTaskService;
+package ru.rrozhkov.easykin.ws.task;
 
 import javax.xml.ws.Endpoint;
 
-public class EasyKinReservePublisher {
+public class EasyKinTaskPublisher {
 	public static void main(String[] args){
 		System.out.println("Publish EasyKinService: http://172.31.46.6:8082/EasyKinTaskService/");
 		Endpoint.publish("http://172.31.46.6:8082/EasyKinTaskService/", new EasyKinTaskService());
