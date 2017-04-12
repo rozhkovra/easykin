fafd046e28ccb5dd5e602739654bd2bf0790c3e2
lux_rozhkov <rrozhkov@luxoft.com>
2017-04-12 13:08:57 +0600

    Update tasks

--- src/main/java/ru/rrozhkov/easykin/data/impl/stat/StaticServiceCalcDataProvider.java
+++ src/main/java/ru/rrozhkov/easykin/data/impl/stat/StaticServiceCalcDataProvider.java
@@ -46,8 +46,21 @@ public class StaticServiceCalcDataProvider extends CollectionDataProvider<Servic
 				,(ServiceCalc)createServiceCalc(DateUtil.parse("28.02.2017"),
 						Arrays.asList(
 								createDefaultCalc(WATER, MoneyFactory.create(251.34), false)
-								, createDefaultCalc(HOTWATER, MoneyFactory.create(941.19), false)
+								, createDefaultCalc(HOTWATER, MoneyFactory.create(1008.42), false)
 								, createElectricityCalc(15539, 15632, MoneyFactory.create(3.56), MoneyFactory.create(0.0), false)
+								, createGazCalc(0.0, 0.0, MoneyFactory.create(80.06), false)
+								, createDefaultCalc(HEATING, MoneyFactory.create(1520.62), false)
+								, createDefaultCalc(REPAIR, MoneyFactory.create(341.03), false)
+								, createDefaultCalc(ANTENNA, MoneyFactory.create(72.00), false)
+								, createDefaultCalc(INTERCOM, MoneyFactory.create(30.00), false)
+								, createDefaultCalc(HOUSE, MoneyFactory.create(1477.12), true)
+						)
+				)
+				,(ServiceCalc)createServiceCalc(DateUtil.parse("31.03.2017"),
+						Arrays.asList(
+								createDefaultCalc(WATER, MoneyFactory.create(0.00), false)
+								, createDefaultCalc(HOTWATER, MoneyFactory.create(1075.65), false)
+								, createElectricityCalc(15539, 15539, MoneyFactory.create(3.56), MoneyFactory.create(0.0), false)
 								, createGazCalc(0.0, 0.0, MoneyFactory.create(80.06), false)
 								, createDefaultCalc(HEATING, MoneyFactory.create(1520.62), false)
 								, createDefaultCalc(REPAIR, MoneyFactory.create(341.03), false)
@@ -56,7 +69,7 @@ public class StaticServiceCalcDataProvider extends CollectionDataProvider<Servic
 								, createDefaultCalc(HOUSE, MoneyFactory.create(1477.12), false)
 						)
 				)
-				,(ServiceCalc)createServiceCalc(DateUtil.parse("31.03.2017"),
+				,(ServiceCalc)createServiceCalc(DateUtil.parse("30.04.2017"),
 						Arrays.asList(
 								createDefaultCalc(WATER, MoneyFactory.create(0.00), false)
 								, createDefaultCalc(HOTWATER, MoneyFactory.create(941.19), false)
@@ -69,7 +82,7 @@ public class StaticServiceCalcDataProvider extends CollectionDataProvider<Servic
 								, createDefaultCalc(HOUSE, MoneyFactory.create(1477.12), false)
 						)
 				)
-			);
+				);
 	public StaticServiceCalcDataProvider() {
 		super(CollectionUtil.copy(serviceCalcs));
 	}
