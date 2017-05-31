1c433eb0e8c5bee97dd832c910c2950fa8a979d5
lux_rozhkov <rrozhkov@luxoft.com>
2017-05-31 13:33:50 +0600

    EKIN-0006 Change equals method

--- src/main/java/ru/rrozhkov/easykin/model/task/impl/Task.java
+++ src/main/java/ru/rrozhkov/easykin/model/task/impl/Task.java
@@ -88,8 +88,6 @@ public class Task implements ITask, Comparable<ITask>{
 		Task task = (Task)obj;
 		return id==task.getId()
 				&& name.equals(task.getName())
-				&& createDate.equals(task.getCreateDate())
-				&& planDate.equals(task.getPlanDate())
 				&& priority.equals(task.getPriority())
 				&& category.equals(task.getCategory())
 				&& status.equals(task.getStatus());
