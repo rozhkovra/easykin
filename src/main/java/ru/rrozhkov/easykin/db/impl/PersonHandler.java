f036df9e6f09730a686135209fe5f64f97545bb1
lux_rozhkov <rrozhkov@luxoft.com>
2017-06-06 10:43:39 +0600

    EKIN-0011 Update service for perso

--- src/main/java/ru/rrozhkov/easykin/db/impl/PersonHandler.java
+++ src/main/java/ru/rrozhkov/easykin/db/impl/PersonHandler.java
@@ -4,12 +4,13 @@ import java.sql.SQLException;
 import java.util.Collection;
 
 import ru.rrozhkov.easykin.model.person.IPerson;
+import ru.rrozhkov.easykin.model.person.impl.convert.AuthDBPersonConverter;
 import ru.rrozhkov.easykin.model.person.impl.convert.DBPersonConverter;
 
 public class PersonHandler {
 	public static String select = "select * from PERSON";
 	
 	public static Collection<IPerson> select() throws SQLException {
-		return EasyKinDBManager.instance().select(select,new DBPersonConverter());
+		return EasyKinDBManager.instance().select(select,new AuthDBPersonConverter());
 	}
 }
