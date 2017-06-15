8f3817ee3e7c6281d80e30bbba33fc3cbfcd9240
lux_rozhkov <rrozhkov@luxoft.com>
2017-06-15 13:02:41 +0600

    EKIN-0014 Error in getCurrentCategory after reload

--- src/main/java/ru/rrozhkov/easykin/gui/util/ContextUtil.java
+++ src/main/java/ru/rrozhkov/easykin/gui/util/ContextUtil.java
@@ -9,6 +9,7 @@ import ru.rrozhkov.lib.util.DateUtil;
 public class ContextUtil {
 	public static ICategory getCurrentCategory(MasterDataContext context, JTabbedPane tabs){
 		int index = tabs.getSelectedIndex();
+		if(index==-1)return null;
 		String categoryName = tabs.getTitleAt(index);
 		for(ICategory category : context.categories()){
 			if(categoryName.equals(category.getName()))
