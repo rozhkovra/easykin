289801bcca501d811017a89f6a7b759db373846e
lux_rozhkov <rrozhkov@luxoft.com>
2017-07-06 12:53:15 +0600

    EKIN-0027 Add Icon to Service window

--- bin/jar.bat
+++ bin/jar.bat
@@ -1,5 +1,5 @@
 ﻿REM
 cd ..\target
-copy  easykin-5.0.1-SNAPSHOT.jar ..\..\easykin\lib
-copy  easykin-5.0.1-SNAPSHOT.jar ..\..\easykin.android\app\libs
-copy  easykin-5.0.1-SNAPSHOT.jar ..\..\easykin.web\src\main\webapp\WEB-INF\lib
+copy  easykin-5.0.2.jar ..\..\easykin\lib
+copy  easykin-5.0.2.jar ..\..\easykin.android\app\libs
+copy  easykin-5.0.2.jar ..\..\easykin.web\src\main\webapp\WEB-INF\lib
