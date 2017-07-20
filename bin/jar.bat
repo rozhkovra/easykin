833b01eb9dc2cba3f5e75703a27b4fd602d33720
Roman Rozhkov <rrozhkov@luxoft.com>
2017-07-20 15:38:10 +0600

    EKIN-0035 Release 5.0.3

--- bin/jar.bat
+++ bin/jar.bat
@@ -1,5 +1,5 @@
 ﻿REM
 cd ..\target
-copy  easykin-5.0.2.jar ..\..\easykin\lib
-copy  easykin-5.0.2.jar ..\..\easykin.android\app\libs
-copy  easykin-5.0.2.jar ..\..\easykin.web\src\main\webapp\WEB-INF\lib
+copy  easykin-5.0.3.jar ..\..\easykin\lib
+copy  easykin-5.0.3.jar ..\..\easykin.android\app\libs
+copy  easykin-5.0.3.jar ..\..\easykin.web\src\main\webapp\WEB-INF\lib
